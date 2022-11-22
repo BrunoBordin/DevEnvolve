b681982dd5fa4d0243bc143d16698f0b6e0847d9
Bruno Bordin <bruno.bordin@hammerconsult.com.br>
2022-11-22 17:04:28 -0300

    - Add conexão com localdb - Add objeto Freelancer para teste - Add endpoints buscar freelancer e lista de freelancers

---
+++ Api.DevEnvolve/Api.DevEnvolve/Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using Api.DevEnvolve.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.DevEnvolve.Data
+{
+    public class DataContext : DbContext
+    {
+        public virtual DbSet<Freelancer> Feelancer { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json")
+               .Build();
+
+            var connectionString = configuration.GetConnectionString("DevEnvolve");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Freelancer>(x => x.ToTable("Freelancer"));
+        }
+    }
+}
---
+++ Api.DevEnvolve/Api.DevEnvolve/Repository/FreelancerRepository.cs
@@ -0,0 +1,42 @@
+﻿using Api.DevEnvolve.Data;
+using Api.DevEnvolve.Model;
+
+namespace Api.DevEnvolve.Repository
+{
+    public class FreelancerRepository
+    {
+        public static List<Freelancer> GetFreelancers()
+        {
+            try
+            {
+                using (var dbContext = new DataContext())
+                {
+                    List<Freelancer> freelancers = dbContext.Feelancer.AsQueryable().ToList();
+
+                    return freelancers;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static Freelancer GetFreelancerById(int idFreelancer)
+        {
+            try
+            {
+                using (var dbContext = new DataContext())
+                {
+                    Freelancer freelancer = dbContext.Feelancer.AsQueryable().Where(x => x.idFreelancer == idFreelancer).FirstOrDefault();
+
+                    return freelancer;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
